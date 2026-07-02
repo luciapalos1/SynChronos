14e69a802573cb047e803c7abfaf2a5d5f53a819
Palos L. <luciampalos@gmail.com>
2026-07-02 19:11:17 -0300

    Add files via upload

---
+++ SynChronos/src/main/java/com/SynChronos/SynChronosApplication.java
@@ -0,0 +1,13 @@
+package com.SynChronos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynChronosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynChronosApplication.class, args);
+	}
+
+}
