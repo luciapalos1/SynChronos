46d2500b1ceb39a7163b2474f6b3144b23a00694
lucia <luciampalos@gmail.com>
2026-07-13 14:28:55 -0300

    registrar usuario nuevo en Service usuario

--- SynChronos/src/main/java/com/SynChronos/features/usuario/service/UsuarioService.java
+++ SynChronos/src/main/java/com/SynChronos/features/usuario/service/UsuarioService.java
@@ -1,5 +1,13 @@
 package com.SynChronos.features.usuario.service;
 
+import com.SynChronos.features.usuario.Role;
+import com.SynChronos.features.usuario.dtos.CredencialDto;
+import com.SynChronos.features.usuario.dtos.UsuarioNewDto;
+import com.SynChronos.features.usuario.dtos.UsuarioResponseDto;
+import com.SynChronos.features.usuario.mapper.CredencialMapper;
+import com.SynChronos.features.usuario.mapper.UsuarioMapper;
+import com.SynChronos.features.usuario.model.CredencialEntity;
+import com.SynChronos.features.usuario.model.UsuarioEntity;
 import com.SynChronos.features.usuario.repository.UsuarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,6 +16,21 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class UsuarioService {
     private UsuarioRepository usuarioRepository;
+    private UsuarioMapper usuarioMapper;
+    private CredencialMapper credencialMapper;
+
+    public UsuarioResponseDto registrarUsuario(UsuarioNewDto usuarioNuevo, CredencialDto credencialNueva){
+        UsuarioEntity usuario = usuarioMapper.toEntity(usuarioNuevo);
+        usuario.setActivo(true);
+
+        CredencialEntity credencial = credencialMapper.toEntity(credencialNueva);
+        credencial.setRole(Role.ROLE_USUARIO);
+        usuario.setCredencial(credencial);
+
+        usuario.setCredencial(credencial);
+
+        return usuarioMapper.toResponseDto(usuarioRepository.save(usuario));
+    }
 
 
 }
